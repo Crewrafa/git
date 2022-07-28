1c367c443b48e1bc3b49b55a76dba172afb61904
Rafa Báez <psicologorafaelbaez@gmail.com>
2022-07-27 19:31:42 -0500

    otra verison

--- file2.py
+++ file2.py
@@ -1,13 +1,5 @@
 def run():
     pass
-hola = int(input('Escribe 1 o 2: '))
-if hola == 1:
-    print('Cara de monda tan obediente')
-elif hola == 2:
-    print('Excelente 2')
-else:
-    print('re f')
-hola  
 
 if __name__ == "__main__":
     run()
